622378480ae0ff3afe0ccf13846c92a3ce031b63
ravidro1 <ravidrrt@gmail.com>
2023-04-24 14:07:30 +0300

    fix Responsive

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ function App() {
   return (
     <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center">
       <Routes>
-        {/* <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/List/:username" element={<ToDoList />} />
-      <Route path="/SignUp" element={<SignUp />} /> */}
-        <Route path="/" element={<ToDoList />} />
+        <Route path="/SignUp" element={<SignUp />} />
       </Routes>
     </div>
   );
